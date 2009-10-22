99c49716a453f83359adc07015991f3e71e72746
Eli Van Zoeren <elivz@EliVZs-MacBook.local>
2009-10-21 22:48:28 -0400

    Show only downloads for a particular reader

--- app/controllers/admin/downloads_controller.rb
+++ app/controllers/admin/downloads_controller.rb
@@ -1,5 +1,12 @@
 class Admin::DownloadsController < Admin::ResourceController
+  
+  def index
+    @readers = Reader.find(:all)
     
-    @downloads = find_downloads_by_reader(:reader)
+    if params[:reader]
+      @user = params[:reader]
+      @downloads = Download.find_all_by_reader_id(params[:reader])
+    end
+  end
     
 end

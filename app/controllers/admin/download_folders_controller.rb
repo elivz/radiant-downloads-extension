436124b7c90f46a5e027d2a33c548bd753314b66
Eli Van Zoeren <elivz@EliVZs-MacBook.local>
2009-11-21 19:28:33 -0500

    Uploads working again. Redirect on save.

--- app/controllers/admin/download_folders_controller.rb
+++ app/controllers/admin/download_folders_controller.rb
@@ -3,5 +3,18 @@ class Admin::DownloadFoldersController < Admin::ResourceController
   def index
     @readers = Reader.find(:all)
   end
-    
+  
+  def new
+    @download_folder.reader_id = params[:reader_id]
+  end
+  
+  def create
+    @download_folder = DownloadFolder.new(params[:download_folder])
+    if @download_folder.save
+      redirect_to :controller => 'admin/downloads', :action => 'index', :reader => @download_folder.reader_id
+    else
+      render :action => "new"
+    end
+  end
+  
 end
--- app/controllers/admin/downloads_controller.rb
+++ app/controllers/admin/downloads_controller.rb
@@ -9,5 +9,19 @@ class Admin::DownloadsController < Admin::ResourceController
       @folders = DownloadFolder.find_all_by_reader_id(params[:reader])
     end
   end
+  
+  def new
+    @download.reader_id = params[:reader_id]
+    @download.download_folder_id = params[:download_folder_id]
+  end
+  
+  def create
+    @download = Download.new(params[:download])
+    if @download.save
+      redirect_to :controller => 'admin/downloads', :action => 'index', :reader => @download.reader_id
+    else
+      render :action => "new"
+    end
+  end
     
 end
